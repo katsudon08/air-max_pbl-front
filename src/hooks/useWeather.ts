06c6fb04ad0f8cde6220682997172c7f9e0b917d
matsu <matsuaki171101@gmail.com>
2024-07-31 00:18:24 +0900

    トーストの実装

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -2,18 +2,27 @@ import { useRoom } from "@/context/roomContext"
 import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import { useWeatherAPI } from "./useWeatherAPI"
 import { Weather } from "@/types/weather"
+import { handleIsWeatherCaution } from "@/utils/handleIsWeatherCaution"
+import { useToast } from "@/components/ui/use-toast"
 
 type SetData = Dispatch<SetStateAction<Weather>> | Dispatch<SetStateAction<Weather[]>>
 
 export const useWeather = (setData: SetData, isLatest: boolean) => {
     const { room, setRoom } = useRoom()
     const { get, post } = useWeatherAPI(isLatest)
+    const { toast } = useToast()
 
     useEffect(() => {
-        console.log(room)
         const handleRequest = async () => {
             const postData = await post(room)
             const getData = await get(room)
+
+            if (handleIsWeatherCaution(getData, isLatest)) {
+                toast({
+                    variant: "destructive",
+                    title: "快適な温度を保つために、冷房のご使用をお勧めいたします"
+                })
+            }
 
             console.log(postData)
             setData(getData)
